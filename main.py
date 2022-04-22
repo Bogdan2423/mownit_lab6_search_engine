1838013d3b645befae918595ddddd0f88f87aba2
Mateusz Wronka <matwro2001@gmail.com>
2022-04-22 20:43:33 +0200

    matrix generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from english_words import english_words_lower_set
+from scipy import sparse
 
 library_dir = "./wiki"
 
@@ -27,7 +28,24 @@ def read_dictionary():
     return word_list
 
 
-make_dictionary(library_dir)
-di = read_dictionary()
-print(di)
-print(len(di))
+def make_matrix(library_dir, dictionary_list):
+    matrix = sparse.dok_matrix((len(dictionary), len(os.listdir(library_dir))))
+    dictionary_dict = dict()
+    for index, word in enumerate(dictionary_list):
+        dictionary_dict[word] = index
+
+    for file_num, filename in enumerate(os.listdir(library_dir)):
+        print(file_num)
+        with open(library_dir + "/" + filename, 'r', encoding="latin-1") as f:
+            wordlist = f.read().split()
+            for word in wordlist:
+                stripped_word = word.strip(".,!?\"'-:;()[]{} ").lower()
+                if stripped_word in dictionary_dict.keys():
+                    matrix[dictionary_dict[stripped_word], file_num] += 1
+
+    sparse.save_npz("matrix", matrix.tocsc(), False)
+
+
+#make_dictionary(library_dir)
+dictionary = read_dictionary()
+#make_matrix(library_dir, dictionary)
